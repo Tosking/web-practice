e0aa41686de41fb34b357874fe4a95a6811ec74d
Tosking <tosking@duck.com>
2023-09-11 00:45:10 +0500

    added registration modal window

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
- 
+const signIn = document.querySelector(".header__login")
+const signUp = document.querySelector(".header__signup")
+const modal = document.querySelector(".modal_container")
+const modalClose = document.querySelector(".modal__close")
+const headerText = document.querySelector(".modal_header__text")
+const modalSignIn = document.querySelector(".modal__login")
+const modalSignup = document.querySelector(".modal__signup")
+
+signIn.addEventListener("click", () => {
+    modal.style.display = "block"
+    modalSignIn.style.display = "inline"
+    headerText.innerHTML = "Вход"
+})
+signUp.addEventListener("click", () => {
+    modal.style.display = "block"
+    modalSignup.style.display = "inline"
+    headerText.innerHTML = "Регистрация"
+})
+
+modalClose.addEventListener("click", () => {
+    modal.style.display = "none"
+    modalSignIn.style.display = "none"
+    modalSignup.style.display = "none"
+})
