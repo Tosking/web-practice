73a42d080c3ad75c6c1dc2e21d56d52afd0f770f
Tosking <tosking@duck.com>
2023-09-15 19:23:53 +0500

    added login function

--- script.js
+++ script.js
@@ -5,20 +5,54 @@ const modalClose = document.querySelector(".modal__close")
 const headerText = document.querySelector(".modal_header__text")
 const modalSignIn = document.querySelector(".modal__login")
 const modalSignup = document.querySelector(".modal__signup")
+const signinSubmit = document.querySelector(".login__submit")
+const signupSubmit = document.querySelector(".signup__submit")
 
-signIn.addEventListener("click", () => {
-    modal.style.display = "block"
-    modalSignIn.style.display = "inline"
-    headerText.innerHTML = "Вход"
-})
-signUp.addEventListener("click", () => {
-    modal.style.display = "block"
-    modalSignup.style.display = "inline"
-    headerText.innerHTML = "Регистрация"
-})
+function deleteAllCookies() {
+    const cookies = document.cookie.split(";");
 
+    for (let i = 0; i < cookies.length; i++) {
+        const cookie = cookies[i];
+        const eqPos = cookie.indexOf("=");
+        const name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
+        document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
+    }
+}
+
+if(signIn != null){
+    signIn.addEventListener("click", () => {
+        modal.style.display = "block"
+        modalSignIn.style.display = "inline"
+        headerText.innerHTML = "Вход"
+    })
+    signUp.addEventListener("click", () => {
+        modal.style.display = "block"
+        modalSignup.style.display = "inline"
+        headerText.innerHTML = "Регистрация"
+    })
+}
+else {
+    document.querySelector(".header__exit").addEventListener("click", () => {
+        deleteAllCookies()
+        window.location.href="/"
+    })
+}
 modalClose.addEventListener("click", () => {
     modal.style.display = "none"
     modalSignIn.style.display = "none"
     modalSignup.style.display = "none"
 })
+
+
+
+function validateSignUp(){
+    if(document.querySelector(".signup__password").value != document.querySelector(".signup__password__2").value){
+        document.querySelector(".signup__password__2").style.backgroundColor = "#f99"
+        return false
+    }
+    console.log("123")
+    return true
+}
+
+document.querySelector(".signup__form").onsubmit = validateSignUp
+
--- server.js
+++ server.js
@@ -1,19 +1,83 @@
-const express = require('express');
+const express = require('express')
 const fs = require('fs')
 const path = require('path');
+const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser');
+const { createHash } = require('crypto');
 const app = express()
 const port = 3000
+require('dotenv').config()
+app.set('view engine', 'ejs');  
+
+const { Client } = require('pg');
+const pg = new Client({
+    host: process.env.PG_HOST,
+    port: process.env.PG_PORT,
+    user: process.env.PG_USER,
+    password: process.env.PG_PASSWORD,
+    database: process.env.PG_DATABASE,
+    ssl: false,
+})
+pg.connect()
 
 app.use("/style", express.static(path.join(__dirname, 'style')))
-
-console.log("style")
+app.use(cookieParser());
+app.use( bodyParser.json() );    
+app.use(bodyParser.urlencoded({     
+  extended: true
+})); 
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/index.html'))
+    console.log(req.cookies)
+    res.render("index", {
+        name: req.cookies.name,
+        id: req.cookies.id
+    })
 })
 
 app.get('/script.js', (req, res) => {
     res.sendFile(path.join(__dirname, '/script.js'))
+})
+
+app.get('/auth/redirect.js', (req, res) => {
+    res.sendFile(path.join(__dirname, '/redirect.js'))
+})
+
+app.post("/auth/signin", (req, res) => {
+    const pass = createHash('sha256').update(req.body.password).digest('hex')
+    const result = pg.query(`SELECT id, name FROM users WHERE password = '${pass}' AND email = '${req.body.email}'`, (err, result) => {
+        if(result == undefined){
+            res.redirect("/")
+            return
+        }
+        else {
+            res.cookie('cookie',randomNumber, { maxAge: 90000, httpOnly: true });
+            res.cookie('id', result.rows[0].id)
+            res.cookie('name', result.rows[0].name)
+            res.redirect("/")
+            return
+        }
+    })
+})
+
+app.post("/auth/signup", (req, res) => {
+    if(req.body.name != null && req.body.password != null && req.body.email.match("^[a-zA-Z0-9]+(?:\.[a-zA-Z0-9]+)*@[a-zA-Z0-9]+(?:\.[a-zA-Z0-9]+)*$")){
+        const pass = createHash('sha256').update(req.body.password).digest('hex')
+        pg.query(`SELECT email FROM users WHERE email = '${req.body.email}'`, (err, result) => {
+            if(result == undefined){
+                pg.query(`INSERT INTO users (name, email, password) VALUES ('${req.body.name}', '${req.body.email}', '${pass}') RETURNING id, name;`, (err, result) => {
+                    const randomNumber=createHash('sha256').update(Math.random().toString()).digest('hex')
+                    res.cookie('cookie',randomNumber, { maxAge: 90000, httpOnly: true });
+                    res.cookie('id', result.rows[0].id)
+                    res.cookie('name', result.rows[0].name)
+                    return
+                })
+            }
+
+        })
+    }
+    res.redirect("/")
+    return
 })
 
 
